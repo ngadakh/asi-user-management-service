ebc106b1232d3e5a895f9b662ebcbe7d33f87725
Navnath <61001142+ngadakh@users.noreply.github.com>
2024-03-09 01:32:15 +0530

    add proto changes for user management service

--- cmd/asi-user-management/main.go
+++ cmd/asi-user-management/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/ngadakh/asi-user-management-service/generated/go/asi-user-management-service"
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func (s *server) CreateUser(ctx context.Context, in *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+	log.Printf("CreateUser called with request: %v", in)
+
+	return &pb.CreateUserResponse{
+		Id:    "123",
+		Name:  "Navnath Gadakh",
+		Email: "navnathgadakh@gmail.com",
+	}, nil
+}
 
 func main() {
 	fmt.Println("Hello World!!")
+
+	listen, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterUserServiceServer(s, &server{})
+	if err := s.Serve(listen); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
